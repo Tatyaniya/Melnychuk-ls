9bff1d2ec1b20e97c93cc3e85988e9df06158e97
Tatyaniya <afromyak13@gmail.com>
2021-04-04 13:46:42 +0300

    components

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,37 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+    .app-container
+        headline(title="Панель администрирования")
+            user
+        navigation
+        .page-content
+            .container
+                .header
+                    .title Блок "Обо мне"
+                    iconed-btn(type="iconed" title="Добавить группу")
 </template>
+
+<script>
+import headline from "./components/headline";
+import user from "./components/user";
+import navigation from "./components/navigation";
+import button from "./components/button";
+
+export default {
+    components: {
+        headline,
+        user,
+        navigation,
+        iconedBtn: button,
+    }
+};
+
+</script>
+
+<style lang="postcss">
+    @import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800");
+    @import "normalize.css";
+    @import "../styles/mixins.pcss";
+    @import "../styles/layout/base.pcss";
+</style>
+
+<style lang="postcss" scoped src="./app.pcss"></style>
--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -16,3 +16,17 @@ menuListHam.addEventListener('click', function(e) {
         menuHamburger.style.display = 'none'; 
     }
 });
+
+// плавная прокрутка
+const anchors = document.querySelectorAll('a[href^="#"]');
+
+for(let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault()
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+}
